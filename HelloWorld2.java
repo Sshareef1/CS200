7f4a9b66ef74019aa23a3875e3e70ad1f09dd017
Sshareef1 <101297611+Sshareef1@users.noreply.github.com>
2022-09-22 15:57:37 -0500

    Update HelloWorld2.java

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -6,7 +6,7 @@ public class HelloWorld2
     {
         Scanner myAge = new Scanner(System.in);
         //Declare Scanner object for age
-        System.out.println("Hello World!"+ "\nMy Nameis D. Dang");
+        System.out.println("Hello World!"+ "\nMy Nameis S. Shareef");
         //system.out.println() is to display output to the screen
         //\n to add a newline
 
@@ -17,7 +17,7 @@ public class HelloWorld2
         int age = myAge.nextInt();
 
         //Input age
-        System.out.println("Your name is: D. Dang, and your age is: " + age);
+        System.out.println("Your name is: S. Shareef, and your age is: " + age);
 //Print out user's name and age
     }
 }
