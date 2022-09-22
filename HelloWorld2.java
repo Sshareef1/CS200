e96bf447de80d8f1a5782e50ef07ae9c13978b3f
Sshareef1 <101297611+Sshareef1@users.noreply.github.com>
2022-09-22 15:56:33 -0500

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+//Import Scanner class
+public class HelloWorld2
+{
+    public static void main(String args[ ])
+    {
+        Scanner myAge = new Scanner(System.in);
+        //Declare Scanner object for age
+        System.out.println("Hello World!"+ "\nMy Nameis D. Dang");
+        //system.out.println() is to display output to the screen
+        //\n to add a newline
+
+        //text between quotes is called a string literal
+        System.out.print("Please enter your age(integer number only): ");
+
+        //Prompt a message to ask for age
+        int age = myAge.nextInt();
+
+        //Input age
+        System.out.println("Your name is: D. Dang, and your age is: " + age);
+//Print out user's name and age
+    }
+}
