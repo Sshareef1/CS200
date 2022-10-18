1108065d204cde6d09a6875b415fe75b6f5d7141
Sshareef1 <101297611+Sshareef1@users.noreply.github.com>
2022-10-18 15:18:14 -0500

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,22 @@
+public class MultiplicationTable{
+public static void main(String[]args)
+{
+
+int y;
+
+int x;
+
+int prod;
+
+for(x=1;  x<=12;  x++){  
+      for(y=1; y<=12;y++){
+      
+    prod=x * y;
+    System.out.printf("%5d", prod);    
+                                        }      
+    
+    
+    System.out.println();                  }
+          
+        }        
+       }
