804ff702af7fda9bf3ca4300d931c3b95adff277
Sshareef1 <101297611+Sshareef1@users.noreply.github.com>
2022-10-20 13:52:15 -0500

    Update scoring.java

--- scoring.java
+++ scoring.java
@@ -16,34 +16,53 @@ as it marks the start/entry point of the program when executed),
 arguments/parameters: ( ) data to be sent to the method - 
 (more details when methods are covered)
 ---------------------------------------------------------------------------------*/        
-public static void main(String args[ ]) 
-        {
+   public static void main(String args[ ]) 
+   {
                //create any objects if needed (i.e. scanner for keyboard input)
-               Scanner s = new Scanner(System.in);
-
+      Scanner s = new Scanner(System.in);
+   
               //declare data storage space (constants and variables)
-              int nofS;
-              String name;
-              double score;
-              int hs;
-              String best;
+      int nofS;
+      String name;
+      double score;
+      double hs = 0;
+      String best = "";
               
                  
-
+   
               //get  input as required by program specifications
-              System.out.print("enter ur nofs: ");
-              nofS = s.nextLine();
+      System.out.print("enter ur nofs: ");
+      nofS = s.nextInt();
               
-               System.out.print("enter ur best: ");
-               best = s.nextLine(); 
-               
-                System.out.print("enter ur hs: ");
-                hs = s.nextLine();
-
+ // // System.out.print("enter ur best: ");
+// //       best = s.nextLine(); 
+// //                
+// //       System.out.print("enter ur hs: ");
+// //       hs = s.nextDouble();
+                
+      while( nofS>0) {
+      System.out.print("enter ur name: ");
+      name = s.next();
+      
+      System.out.print("enter ur score: ");
+      score = s.nextDouble();
+     
+     if(score> hs){
+      hs = score;
+      best = name;
+      }//close
+      
+    
+      nofS--;
+      }
+      
+      System.out.print("The highest score is: " + hs+ " The winner name is: " +best);
+      
+   
               //process data as required by program specifications
-
+   
               //display results as required by program specifications
-				  
-				  
-         } //closing main method
+   			  
+   			  
+   } //closing main method
 } //closing class header
